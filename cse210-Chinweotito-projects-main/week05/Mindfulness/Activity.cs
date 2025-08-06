d65b6072c7253a6cd119d09c7db13dddad4267e5
Emmychizzy042 <emmanuelbenjamin206@gmail.com>
2025-08-06 04:50:22 -0700

    Fixed submodule issue: added Mindfulness folder as normal content

---
+++ cse210-Chinweotito-projects-main/week05/Mindfulness/Activity.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading;
+
+public class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"--- {_name} ---\n");
+        Console.WriteLine($"{_description}\n");
+        Console.Write("Enter duration in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nGet ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(3);
+        Console.WriteLine($"\nYou have completed {_duration} seconds of the {_name}.\n");
+        ShowSpinner(2);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[i % spinner.Length]);
+            Thread.Sleep(200);
+            Console.Write("\b");
+            i++;
+        }
+    }
+
+    public void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
